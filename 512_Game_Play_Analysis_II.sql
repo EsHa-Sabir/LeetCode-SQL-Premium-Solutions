f385049d74a512dbf30c55b7186a62539a82788f
Esha Sabir <e.s.journey456@gmail.com>
2026-08-24 13:17:15 +0500

    Add problem statement for Game Play Analysis II
    Added problem statement and expected output for SQL query.

--- 512_Game_Play_Analysis_II.sql
+++ 512_Game_Play_Analysis_II.sql
@@ -1,4 +1,39 @@
-with GetDeviceId AS (
-  Select player_id,device_id,Row_Number()over(Partition by player_id order by event_date)as rnk from Activity 
+/*
+
+### LeetCode 512: Game Play Analysis II (🔒 Premium / Easy)
+
+Problem Statement:
+Write an SQL query to report the device that each player logged in with for
+the first time. 
+
+Table: Activity
++--------------+---------+ 
+
+| Column Name  | Type    |
++--------------+---------+ 
+
+| player_id    | int     |
+| device_id    | int     |
+| event_date   | date    |
+| games_played | int     |
++--------------+---------+
+(player_id, event_date) is the primary key of this table. 
+
+### Expected Output:
+Return a table with 'player_id' and their corresponding first 'device_id'.
+
+*/ 
+
+WITH GetDeviceId AS (
+SELECT
+player_id,
+device_id,
+ROW_NUMBER() OVER(PARTITION BY player_id ORDER BY event_date ASC) AS rnk
+FROM Activity
 )
-select player_id,device_id from GetDeviceId where rnk=1
+SELECT
+player_id,
+device_id
+FROM GetDeviceId
+WHERE rnk = 1;
+
